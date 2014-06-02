426a448686fc5ef51b6a6f23895d4f83ad71f446
Simon Taddiken <simon.taddiken@gmail.com>
2014-06-02 10:51:19 +0200

    documentation for ResourceKey

--- src/main/java/de/skuzzle/stringz/ResourceKey.java
+++ src/main/java/de/skuzzle/stringz/ResourceKey.java
@@ -5,8 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * When using the default field mapping strategy, this annotation can be used to 
+ * explicitly specify the resource key to which a variable should be mapped. If this 
+ * annotation is not present on a field, its name is used as key.
+ *  
+ * @author Simon Taddiken
+ * @see DefaultFieldMapper
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ResourceKey {
+    /**
+     * Specifies the key which references the value within a 
+     * {@link java.util.ResourceBundle} taht should be assigned to the annotated field.
+     * @return The resource key to use.
+     */
     public String value();
 }
